53ad345103d0afd79fbbd37e7447aa98ed6f3b72
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-13 02:36:31 +0000

    update w/ solution

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-Leetcode 278-First Bad Version
-Instructions: You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+// Leetcode 278-First Bad Version
+// Instructions: You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
-Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 
-You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+// You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
 
 /**
  * Definition for isBadVersion()
@@ -25,8 +25,26 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        
+      
+  
+        let start = 0;
+        let end = n;
+
+        while (start <= end) {
+            const mid = Math.floor((start + end) / 2);
+            if (isBadVersion(mid)) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return end + 1;
+    };
 };
+
+
+
 
 
     // true bad version versus false(good version)
